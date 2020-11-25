d4bcb5b98506d3536fd59854164e12f74c2404dc
Ivan Klyushin <juice_biz@mail.ru>
2020-11-25 14:08:03 +0300

    first commit

---
+++ lessons/js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+console.log('arr' + " - object");
+console.log(4 + +"5");
+
+let incr = 10;
+    decr = 10;
+
+
+
+console.log(incr++);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2*4 == '8');
